216835f1c045c040ff3356e2b42ff2ccc0980dc7
BernardaP <btp210@gmail.com>
2021-05-05 14:04:31 -0600

    creates a new route for all the paths for categories

---
+++ controllers/categories.js
@@ -0,0 +1,35 @@
+let express = require('express')
+let db = require('../models')
+
+let router = express.Router()
+
+
+//GET /categories - show all the categories that exist
+router.get('/', (req, res) => {
+  db.category.findAll()
+  .then((categories) => {
+    res.render('categories/index', { categories: categories })
+    console.log(categories)
+  })
+  .catch((error) => {
+    console.log('Error in GET /', error)
+    res.status(400).render('main/404')
+  })
+})
+
+//GET GET /categories/:id - show a specific category and all the projects with that category
+router.get('/:id', (req, res) => {
+  db.category.findOne({
+    where: {
+      id: req.params.id
+    }, include: [db.project]
+  }).then((category) => {
+    res.render('categories/show', {category:category})
+  })
+  .catch((error) => {
+    console.log('Error in GET /', error)
+    res.status(400).render('main/404')
+  })
+})
+
+module.exports = router
