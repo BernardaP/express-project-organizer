ec92cfadf2c5808462874b8906af33f39d03a7f0
BernardaP <btp210@gmail.com>
2021-05-05 14:11:09 -0600

    adds toutes for displaying form , editing a project, delete a project

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,16 +1,32 @@
 let express = require('express')
 let db = require('../models')
 let router = express.Router()
+const methodOverride = require('method-override')
+
+router.use(methodOverride('_method'))
 
 // POST /projects - create a new project
 router.post('/', (req, res) => {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployLink: req.body.deployedLink,
-    description: req.body.description
+  db.project.findOrCreate({
+    where: {
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployLink: req.body.deployedLink,
+      description: req.body.description
+    }
   })
-  .then((project) => {
+  .then(([project, created]) => {
+    db.category.findOrCreate({
+      where:{
+        name: req.body.category,
+      }
+      
+    }).then(function([category,created]){
+      project.addCategory(category).then(function(relationInfo) {
+        console.log(`Project ${project.name} added to ${category.name}`)
+        console.log(relationInfo)
+      })
+    })
     res.redirect('/')
   })
   .catch((error) => {
@@ -30,10 +46,103 @@ router.get('/:id', (req, res) => {
   })
   .then((project) => {
     if (!project) throw Error()
-    res.render('projects/show', { project: project })
+    db.category.findAll()
+    .then((categories)=> {
+      console.log(project)
+    res.render('projects/show', { project: project, categories: categories })
+    })
+    
   })
   .catch((error) => {
     res.status(400).render('main/404')
+  })
+})
+
+
+//GET /projects/edit/:id display form to edit
+// router.get('/edit/:id', (req, res)=> {
+//   db.project.findOne({
+//     where: {id: req.params.id}
+//   }).then(function(foundProject){
+//     res.render('projects/edit', {project: foundProject})
+//   })
+//   .catch((error) => {
+//     res.status(400).render('main/404')
+//   })
+// })
+
+router.get('/edit/:id', async (req, res)=> {
+  try {
+    const foundProject = await db.project.findOne({
+      where: {id: req.params.id}
+    })
+    if (foundProject){
+      res.render('projects/edit', {project: foundProject})
+
+    } else {
+      console.log("Project not found")
+    }
+
+  } catch(error){
+    res.status(400).render('main/404')
+    }
+  
+})
+
+
+
+router.post('/:id', (req, res)=>{
+
+  db.project.findOrCreate({
+    where: {id: req.params.id}
+  })
+  .then(([project, created]) => {
+    console.log(">>>>>><<<<<",req.body)
+    db.category.findOrCreate({
+      
+      where:{
+        name: req.body.category,
+      }
+      
+    }).then(function([category,created]){
+      project.addCategory(category).then(function(relationInfo) {
+        // console.log(`Project ${project.name} added to ${category.name}`)
+        // console.log(relationInfo)
+        res.redirect(`/categories/${category.id}`)
+      })
+    })
+    // res.redirect('/')
+  })
+  .catch((error) => {
+    res.status(400).render('main/404')
+  })
+})
+
+router.put('/:id', (req, res)=> {
+  db.project.findOne({
+    where: {id: req.params.id}
+  }).then(function(foundProject){
+    foundProject.update({
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployedLink: req.body.deployLink,
+      description: req.body.description
+    }).then(function(editedProject){
+      res.redirect(`/projects/${editedProject.id}`)
+    })
+  })
+  .catch((error) => {
+    res.status(400).render('main/404')
+  })
+})
+
+
+//POST delete a prject
+router.delete('/:id', (req, res)=>{
+  db.project.destroy({
+    where: {id:req.params.id}
+  }).then(function(){
+    res.redirect('/')
   })
 })
 
